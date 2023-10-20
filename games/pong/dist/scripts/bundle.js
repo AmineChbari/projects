111d0ec35bcf4e12c406a587695ef3f22e235bdc
amine.chbari.etu <amine.chbari.etu@univ-lille.fr>
2023-10-20 08:19:58 +0200

    clean rep

--- games/pong/dist/scripts/bundle.js
+++
@@ -1,2 +0,0 @@
-/*! For license information please see bundle.js.LICENSE.txt */
-(()=>{var __webpack_modules__={"./src/scripts/main.js":()=>{eval("//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvc2NyaXB0cy9tYWluLmpzLmpzIiwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vcG9uZy8uL3NyYy9zY3JpcHRzL21haW4uanM/Mjk2MyJdLCJzb3VyY2VzQ29udGVudCI6WyIiXSwibWFwcGluZ3MiOiIifQ==\n//# sourceURL=webpack-internal:///./src/scripts/main.js\n")}},__webpack_exports__={};__webpack_modules__["./src/scripts/main.js"]()})();
