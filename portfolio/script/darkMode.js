608da7e9df78b17796309400a1fd20e9adef3830
badmark19 <amine@DESKTOP-QK66G6C>
2023-12-17 13:38:41 +0100

    update portfolio

--- portfolio/script/darkMode.js
+++ portfolio/script/darkMode.js
@@ -1,7 +1,10 @@
-const darkModeButton = document.getElementById("darkModeButton");
-const body = document.body;
+const darkModeButton = document.getElementById('darkModeButton');
+const toggleDarkMode = () => {
+  if (document.body.classList.contains('dark-mode')) {
+    document.body.classList.remove('dark-mode');
+  } else {
+    document.body.classList.add('dark-mode');
+  }
+};
 
-darkModeButton.addEventListener("click", () => {
-body.classList.toggle("darkMode");
-});
-
+darkModeButton.addEventListener('click', toggleDarkMode);
