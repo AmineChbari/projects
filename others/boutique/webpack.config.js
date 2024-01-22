f4e7516a684a88fe5c417b8cd952c7a4ec55731f
amine.chbari.etu <amine@DESKTOP-QK66G6C>
2024-01-22 04:34:55 +0100

    fix webpack

--- others/boutique/webpack.config.js
+++ others/boutique/webpack.config.js
@@ -3,118 +3,115 @@ const webpack = require('webpack');
 const TerserPlugin = require('terser-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const cssLoader = {
+  loader: 'css-loader',
+  options: {
+    importLoaders: 1,
+  },
+};
 
 const PRODUCTION = false;
 const DIST_FOLDER = 'dist';
 
 module.exports = {
-   entry: path.resolve(__dirname, 'src', 'scripts', 'main.js'),
+  entry: {
+    app: path.resolve(__dirname, 'src', 'scripts', 'main.js'),
+  },
 
-   output: {
-      path: path.resolve(__dirname, DIST_FOLDER),
-      filename: 'scripts/bundle.js'
-   },
+  output: {
+    path: path.resolve(__dirname, DIST_FOLDER),
+    filename: '[name].[hash].js',
+    publicPath: '/',
+  },
 
-   mode: (PRODUCTION ? 'production' : 'development'),
-   devtool: (PRODUCTION ? undefined : 'eval-source-map'),
+  mode: (PRODUCTION ? 'production' : 'development'),
+  devtool: (PRODUCTION ? false : 'inline-source-map'),
 
-   devServer: {
-      static: {
-         publicPath: path.resolve(__dirname, DIST_FOLDER),
-         watch: true
+  resolve: {
+    extensions: ['.js', '.jsx', '.css'],
+  },
+
+  devServer: {
+    // ...other devServer options...
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: [
+              '@babel/transform-runtime',
+              require('@emotion/babel-plugin'),
+            ],
+          },
+        },
       },
-      host: 'localhost',
-      port: 9000,
-      open: {
-         app: {
-            name: 'firefox'
-         }
-      }
-   },
 
-   module: {
-      rules: [
-         {
-            test: /\.(m?js$|jsx)/,
-            exclude: /(node_modules)/,
-            use: {
-               loader: 'babel-loader',
-               options: {
-                  presets: [
-                     '@babel/preset-env',
-                     '@babel/preset-react'
-                  ],
-                  plugins: [
-                     '@babel/transform-runtime'
-                  ]
-               }
-            }
-         },
-         {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-         },
-         {
-            test: /\.(png|jpg|gif)/i,
-            use: {
-               loader: 'file-loader',
-               options: {
-                  name: '[name].[ext]',
-                  outputPath: 'images'
-               }
-            }
-         }
-      ]
-   },
+      {
+        test: /\.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          cssLoader,
+        ],
+      },
 
-   plugins: [
-      new webpack.ProgressPlugin(),
-      new HtmlWebpackPlugin({
-         template: path.resolve(__dirname, 'src', 'index.html'),
-         filename: './index.html',
-         hash: true,
-      }),
-      new CopyPlugin({
-         patterns: [
-            {
-               context: path.resolve(__dirname, 'src', 'html'),
-               from: "**/*.html",
-               to: 'html',
-               noErrorOnMissing: true,
-               globOptions: {}
-            },
-            {
-               context: path.resolve(__dirname, 'src', 'images'),
-               from: '**/*',
-               to: 'images/[name][ext]',
-               noErrorOnMissing: true,
-            },
-            {
-               context: path.resolve(__dirname, 'src', 'style'),
-               from: '**/*.css',
-               to: 'style/[name][ext]',
-               noErrorOnMissing: true,
-            },
-            {
-               context: path.resolve(__dirname, 'vendor'),
-               from: '**/*.js',
-               to: 'vendor',
-               noErrorOnMissing: true,
-            },
-         ]
-      }),
-   ],
+      {
+        test: /\.(png|jpg|gif)/i,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[hash].[ext]',
+            outputPath: 'images',
+          },
+        },
+      },
+    ],
+  },
 
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
 
-   // gestion de bibliothèques externes à exclure du bundle, ici cas de React
-   externals: {
-      react: 'React',
-      'react-dom/client': 'ReactDOM'
-   },
+  plugins: [
+    new webpack.ProgressPlugin(),
 
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+      filename: './index.html',
+      hash: true,
+    }),
 
-   optimization: {
-      minimize: true,
-      minimizer: [new TerserPlugin()]
-   }
-}
+    new CopyPlugin({
+      patterns: [
+        {
+          context: path.resolve(__dirname, 'src'),
+          from: '**/*.{html,css,svg,json,ico,png,gif,woff2,eot,ttf}',
+          to: DIST_FOLDER,
+        },
+      ],
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+
+    new TerserPlugin({
+      include: /\.js$|\.jsx$/,
+      exclude: /node_modules/,
+      terserOptions: {
+        compress: {
+          warnings: PRODUCTION ? false : true,
+        },
+      },
+    }),
+  ],
+};
